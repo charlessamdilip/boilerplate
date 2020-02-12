89cb14ad6e14b6ad0a1caa8c2d9f088a498a50b6
Charles Sam Dilip <sam@Charless-MacBook-Pro-2.local>
2020-02-12 12:14:43 +0530

    Corrected indentation in template

--- codingame/codingame-board.cpp
+++ codingame/codingame-board.cpp
@@ -27,89 +27,106 @@ using namespace std;
 
 static int turn = 0;
 
-
 #define Now() chrono::high_resolution_clock::now()
 
-static struct Stopwatch {
+static struct Stopwatch
+{
 	chrono::high_resolution_clock::time_point c_time, c_timeout;
 
 	void setTimeout(int us) { c_timeout = c_time + chrono::microseconds(us); }
 
-	void Start(int timeOutMilliSeconds) { c_time = Now(); setTimeout(timeOutMilliSeconds * 1000); }
+	void Start(int timeOutMilliSeconds)
+	{
+		c_time = Now();
+		setTimeout(timeOutMilliSeconds * 1000);
+	}
 
 	inline bool Timeout() { return Now() > c_timeout; }
 
 	long long EllapsedMilliseconds() { return chrono::duration_cast<chrono::milliseconds>(Now() - c_time).count(); }
 } stopwatch;
 
-
-enum PlayerType {
-  OWN,
-  FRIEND,
-  ENEMY,
+enum PlayerType
+{
+	OWN,
+	FRIEND,
+	ENEMY,
 };
 
 /*
   Point vector to compute 2D planar calaculation.
 */
-class Point2 {
+class Point2
+{
 private:
 	int x;
-  int y;
+	int y;
+
 public:
-	Point2() {
+	Point2()
+	{
 		x = -1;
 		y = -1;
 	}
-	Point2(int x, int y) {
+	Point2(int x, int y)
+	{
 		this->x = x;
 		this->y = y;
 	}
 
-	Point2(const Point2 &other) {
+	Point2(const Point2 &other)
+	{
 		this->x = other.x;
 		this->y = other.y;
 	}
 
-	Point2 operator =(const Point2 &other) {
+	Point2 operator=(const Point2 &other)
+	{
 		this->x = other.x;
 		this->y = other.y;
 		return *(this);
 	}
 
-	Point2 operator +(const Point2 &other) {
+	Point2 operator+(const Point2 &other)
+	{
 		return Point2(x + other.x, y + other.y);
 	}
-  
-  Point2 operator +=(const Point2 &other) {
-    this->x += other.x;
-    this->y += other.y;
+
+	Point2 operator+=(const Point2 &other)
+	{
+		this->x += other.x;
+		this->y += other.y;
 	}
 
-
-	bool operator ==(Point2 &other) {
+	bool operator==(Point2 &other)
+	{
 		return this->x == other.x && this->y == other.y;
 	}
 
-	bool operator !=(Point2 &other) {
+	bool operator!=(Point2 &other)
+	{
 		return !(*this == other);
 	}
 
-	int manhattanDistance(Point2 &other) {
+	int manhattanDistance(Point2 &other)
+	{
 		return abs(x - other.x) + abs(y - other.y);
 	}
 
-  int dist2(Point2 &other) {
-    int diffX = this->x - other.x;
-    int diffY = this->y - other.y;
-		return (diffX * diffX  + diffY * diffY);
+	int dist2(Point2 &other)
+	{
+		int diffX = this->x - other.x;
+		int diffY = this->y - other.y;
+		return (diffX * diffX + diffY * diffY);
 	}
 
-  int dist(Point2 &other) {
+	int dist(Point2 &other)
+	{
 		return sqrt(dist2(other));
 	}
 
-	bool isInsideMap() {
+	bool isInsideMap()
+	{
 		return this->x >= 0 && this->y >= 0 && this->y < BOARD_HEIGHT && this->x < BOARD_WIDTH;
 	}
 };
@@ -117,192 +134,228 @@ public:
 /*
   Any object which is positionable
 */
-class Positionable {
+class Positionable
+{
 private:
-  Point2 position;
+	Point2 position;
+
 public:
-  Positionable(int x, int y) {
-    this->position = Point2(x, y);
-  }
-  Positionable(const Point2 &other) {
-    this->position = Point2(other);
-  }
-  Positionable operator =(const Positionable &other) {
-    this->position = other.position;
-  }
+	Positionable(int x, int y)
+	{
+		this->position = Point2(x, y);
+	}
+	Positionable(const Point2 &other)
+	{
+		this->position = Point2(other);
+	}
+	Positionable operator=(const Positionable &other)
+	{
+		this->position = other.position;
+	}
 };
 
 /*
   The actionable object in the game.
 */
-class Agent: Positionable {
+class Agent : Positionable
+{
 private:
-  int id;
-  bool movable;
-  bool active;
+	int id;
+	bool movable;
+	bool active;
+
 public:
-  Agent():Positionable(-1, -1) {
-    this->movable = true;
-  }
-  Agent(int x, int y, bool movable): Positionable(x, y) {
-    this->movable = movable;
-  }
-  Agent(const Point2 &other, bool movable): Positionable(other) {
-    this->movable = movable;
-  }
-  Agent operator =(const Agent &other) {
-    this->id = other.id;
-    this->movable = other.movable;
-    this->active = other.active;
-  }
+	Agent() : Positionable(-1, -1)
+	{
+		this->movable = true;
+	}
+	Agent(int x, int y, bool movable) : Positionable(x, y)
+	{
+		this->movable = movable;
+	}
+	Agent(const Point2 &other, bool movable) : Positionable(other)
+	{
+		this->movable = movable;
+	}
+	Agent operator=(const Agent &other)
+	{
+		this->id = other.id;
+		this->movable = other.movable;
+		this->active = other.active;
+	}
 };
 
 /*
 Game Player
 */
-class Player {
+class Player
+{
 private:
-  PlayerType type;
-  int agentCount;
-  Agent agents[AGENT_COUNT];
+	PlayerType type;
+	int agentCount;
+	Agent agents[AGENT_COUNT];
+
 public:
-  Player();
-  Player(const PlayerType type, int agentCount, int x[], int y[]) {
-    this->type = type;
-    this->agentCount = agentCount;
-    for (int i = 0; i < agentCount; ++i) {
-      agents[i] = Agent(x[i], y[i], true);
-    }
-  }
-  Player operator =(const Player &other) {
+	Player();
+	Player(const PlayerType type, int agentCount, int x[], int y[])
+	{
+		this->type = type;
+		this->agentCount = agentCount;
+		for (int i = 0; i < agentCount; ++i)
+		{
+			agents[i] = Agent(x[i], y[i], true);
+		}
+	}
+	Player operator=(const Player &other)
+	{
 		this->type = other.type;
 		this->agentCount = other.agentCount;
-		for (int i = 0; i < agentCount; ++i) {
-      this->agents[i] = other.agents[i];
-    }
+		for (int i = 0; i < agentCount; ++i)
+		{
+			this->agents[i] = other.agents[i];
+		}
 	}
-
 };
 
-class Move {
+class Move
+{
 public:
-
 	string move;
-  Point2 position;
-	Move() {
+	Point2 position;
+	Move()
+	{
 		move = "";
 		position = Point2(-1, -1);
 	}
 
-	Move(const string &move) {
+	Move(const string &move)
+	{
 		this->move = move;
 	}
 
-	Move(const string &move, const Point2 position) {
+	Move(const string &move, const Point2 position)
+	{
 		this->move = move;
 		this->position = position;
 	}
 };
 
-class Solution {
+class Solution
+{
 private:
 	float score;
+
 public:
 	Move moves[AGENT_COUNT];
-	Solution() {
+	Solution()
+	{
 		score = -INF;
 	}
-  void cloneTo(Solution &);
-  float getScore() { return this->score;}
-  void setScore(float score) { this->score = score;}
+	void cloneTo(Solution &);
+	float getScore() { return this->score; }
+	void setScore(float score) { this->score = score; }
 };
 
-class GameState {
+class GameState
+{
 private:
-  Player players[PLAYER_COUNT];
-  // Define resources and resource map. resources are the static in the map;
+	Player players[PLAYER_COUNT];
+	// Define resources and resource map. resources are the static in the map;
 public:
-  GameState();
-  void initialize();
-  void startTurn();
-  float computeScore(const Solution &);
-  void getRandomSolution(Solution &);
-  void getBestMove(Solution &);
-  void endTurn(Solution &);
-  void cloneTo(GameState &);
-  void cloneFrom(const GameState &);
+	GameState();
+	void initialize();
+	void startTurn();
+	float computeScore(const Solution &);
+	void getRandomSolution(Solution &);
+	void getBestMove(Solution &);
+	void endTurn(Solution &);
+	void cloneTo(GameState &);
+	void cloneFrom(const GameState &);
 };
 
 GameState backup;
 Solution tempSolution;
 
-void GameState::initialize() {
+void GameState::initialize()
+{
 	stringstream input;
 	int width, height;
 
-	cin >> width >> height;	
-	if (LOG_INPUT) {
-    input << width <<" "<< height << endl;
+	cin >> width >> height;
+	if (LOG_INPUT)
+	{
+		input << width << " " << height << endl;
 		cerr << input.str();
 	}
 }
 
-void GameState::startTurn() {
+void GameState::startTurn()
+{
 	stringstream input;
 
-
-	if (LOG_INPUT) {
-    cerr << input.str();
+	if (LOG_INPUT)
+	{
+		cerr << input.str();
 	}
 }
 
-float GameState::computeScore(const Solution &solution) {
+float GameState::computeScore(const Solution &solution)
+{
 	return 0.0;
 }
 
-
-void GameState::getRandomSolution(Solution &solution) {
-  solution.setScore(this->computeScore(solution));
+void GameState::getRandomSolution(Solution &solution)
+{
+	solution.setScore(this->computeScore(solution));
 }
 
-void GameState::cloneTo(GameState &other) {
-  for (int i = 0; i < PLAYER_COUNT; ++i) {
-    other.players[i] = this->players[i];
-  }
+void GameState::cloneTo(GameState &other)
+{
+	for (int i = 0; i < PLAYER_COUNT; ++i)
+	{
+		other.players[i] = this->players[i];
+	}
 }
 
-void GameState::cloneFrom(const GameState &other) {
-  for (int i = 0; i < PLAYER_COUNT; ++i) {
-    this->players[i] = other.players[i];
-  }
+void GameState::cloneFrom(const GameState &other)
+{
+	for (int i = 0; i < PLAYER_COUNT; ++i)
+	{
+		this->players[i] = other.players[i];
+	}
 }
 
-void Solution::cloneTo(Solution &other) {
-  other.score = score;
-  for (int i = 0; i < AGENT_COUNT; i++) {
-    other.moves[i] = this->moves[i];
-  }
+void Solution::cloneTo(Solution &other)
+{
+	other.score = score;
+	for (int i = 0; i < AGENT_COUNT; i++)
+	{
+		other.moves[i] = this->moves[i];
+	}
 }
 
-
-void GameState::getBestMove(Solution &solution) {
+void GameState::getBestMove(Solution &solution)
+{
 	solution.setScore(-INF);
 	stopwatch.Start(DEF_TIMER);
 	int sims = 0;
 	this->cloneTo(backup);
 
-	while (!stopwatch.Timeout()) {
+	while (!stopwatch.Timeout())
+	{
 		this->getRandomSolution(tempSolution);
-	
+
 		this->cloneFrom(backup);
 		sims++;
 	}
-	if (LOG_INFO) cerr << "sims " << sims << endl;
+	if (LOG_INFO)
+		cerr << "sims " << sims << endl;
 }
 
-
-void GameState::endTurn(Solution &solution) {
-	for (int i = 0; i < AGENT_COUNT; i++) {
+void GameState::endTurn(Solution &solution)
+{
+	for (int i = 0; i < AGENT_COUNT; i++)
+	{
 	}
 }
 
@@ -315,7 +368,8 @@ int main()
 	state.initialize();
 	Solution solution;
 
-	while (1) {
+	while (1)
+	{
 		state.startTurn();
 		state.getBestMove(solution);
 		state.endTurn(solution);
